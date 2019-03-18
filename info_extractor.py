a1a328d531d2355eab122114594ccaf9f5d4126e
markliou <markliou@ms51.url.com.tw>
2019-03-18 20:55:51 +0800

    git ignore list

--- info_extractor.py
+++ info_extractor.py
@@ -12,6 +12,7 @@ X = tf.placeholder("float", [None, 28, 28, 1])
 
 # Building the encoder
 def encoder(x):
+    x = (x / 128) - 1
     layer_1 = tf.layers.conv2d(x, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="en1") #14
     layer_2 = tf.layers.conv2d(layer_1, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="en2") #7
     layer_3 = tf.layers.conv2d(layer_2, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="en3") #4
@@ -24,10 +25,9 @@ def encoder(x):
 def decoder(x):
     layer_1 = tf.layers.conv2d_transpose(x, 32, 5, strides = 2, padding = "VALID", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="de1") #7
     layer_2 = tf.layers.conv2d_transpose(layer_1, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="de2") #14
-    layer_3 = tf.layers.conv2d_transpose(layer_2, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.sigmoid, kernel_initializer=tf.keras.initializers.glorot_normal, name="de3") #28
+    layer_3 = tf.layers.conv2d_transpose(layer_2, 32, 5, strides = 2, padding = "SAME", activation=tf.nn.tanh, kernel_initializer=tf.keras.initializers.glorot_normal, name="de3") #28
     
-    return layer_3
-
+    return (layer_3 + 1) * 128
 # Construct model
 encoder_op = encoder(X)
 decoder_op = decoder(encoder_op)
@@ -43,4 +43,10 @@ with tf.Session(config=config) as sess:
     # Run the initializer
     saver.restore(sess,'ae')
 
+    for g_i in range(GENERATING_NO):
+        noise = np.random.random([1, 28, 28, 1])
+        noise_y = sess.run(y_pred, feed_dict={X:noise})
+        noise_y = noise_y.reshape([28,28])
+        PIL.Image.fromarray(noise_y, "P").save('noise_y/{}.png'.format(g_i))
+        PIL.Image.fromarray(noise, "P").save('noise/{}.png'.format(g_i))
 
